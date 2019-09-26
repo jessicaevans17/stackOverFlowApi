83cbb983cacb41e354e32001b639158f43ca6f05
Michelle Polinsky <michelle.polinsky@gmail.com>
2019-09-26 14:39:33 -0400

    Added things

--- Migrations/20190926174823_AddedDateCreate.Designer.cs
+++ Migrations/20190926174823_AddedDateCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using stackoverflowapi;
@@ -9,9 +10,10 @@ using stackoverflowapi;
 namespace stackoverflowapi.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190926174823_AddedDateCreate")]
+    partial class AddedDateCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
